7225628fcb0109f7ce16ed4c50a06ddf585c7f8f
CristinaGVO <vargasosoriocristina@gmail.com>
2026-01-26 11:03:16 -0800

    Update goalFomr

--- src/components/GoalList/GoalList.jsx
+++ src/components/GoalList/GoalList.jsx
@@ -1,29 +1,27 @@
 import { Link } from "react-router";
 
 const GoalList = ({ goals = [] }) => {
-  return (
-    <main>
-      <h1>Goals</h1>
+    return (
+        <main>
+            <h1>Goals</h1>
 
-      <Link to="/goals/new">Add New Goal</Link>
+            <Link to="/goals/new">Add New Goal</Link>
 
-      <h2>Goal List</h2>
+            <h2>Goal List</h2>
 
-      {goals.length === 0 ? (
-        <p>You dont have goals yet</p>
-      ) : (
-        goals.map((goal) => (
-          <article key={goal._id}>
-            <h2>
-              <Link to={`/goals/${goal._id}`}>{goal.title}</Link>
-            </h2>
-          </article>
-        ))
-      )}
-    </main>
-  );
+            {goals.length === 0 ? (
+                <p>You dont have goals yet</p>
+            ) : (
+                goals.map((goal) => (
+                    <article key={goal._id}>
+                        <h2>
+                            <Link to={`/goals/${goal._id}`}>{goal.title}</Link>
+                        </h2>
+                    </article>
+                ))
+            )}
+        </main>
+    );
 };
 
 export default GoalList;
-
-
